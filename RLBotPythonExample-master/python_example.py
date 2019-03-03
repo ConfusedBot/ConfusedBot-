14772f683bda916bc1bebbcf506f9b2b2539bdda
ConfusedBot <48085988+ConfusedBot@users.noreply.github.com>
2019-03-03 14:22:53 -0600

    Update python_example.py

--- RLBotPythonExample-master/python_example.py
+++ RLBotPythonExample-master/python_example.py
@@ -1,5 +1,7 @@
 import math
 import time
+from Util import *
+from States import *
 
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
